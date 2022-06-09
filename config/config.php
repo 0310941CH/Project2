eac9e89154cb4a56e606fc1a63c24044af2ec786
Carlito Hendriks <0310941@student.rocvantwente.nl>
2022-06-09 09:51:00 +0200

    Config map & bestand voor database connectie

---
+++ config/config.php
@@ -0,0 +1,13 @@
+<?php
+$host = "localhost";
+$database = "nu";
+$username = "bit_academy";
+$password = "bit_academy";
+
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$database", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
