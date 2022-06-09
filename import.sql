133498525dc7ebe18d93aa077c4ec6d5254b141e
ThomasMulders <0341091@student.rocvantwente.nl>
2022-06-09 09:42:33 +0200

    snelle sql database gemaakt

--- import.sql
+++ import.sql
@@ -1,10 +1,21 @@
 CREATE DATABASE `nu`;
 
+/* Rank is redacteur of niks */
 CREATE TABLE `users` (
     id MEDIUMINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     voornaam varchar(100) NOT NULL,
     achternaam varchar(100) NOT NULL,
     username char(100) NOT NULL UNIQUE,
     passwords varchar(255) NOT NULL,
-    rank integer(10) NOT NULL
+    rank varchar(100) NOT NULL
+);
+
+CREATE TABLE `berichten` (
+    berichtId NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    titel varchar(100) NOT NULL,
+    samenvatting TINYTEXT NOT NULL,
+    omschrijving LONGTEXT NOT NULL,
+    datum_toegevoegd datetime NOT NULL,
+    datum_aangepast datetime NOT NULL,
+    auteur varchar(100) NOT NULL
 );
