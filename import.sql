73347a1cdbe74030576da9058376e36fd898db7a
Carlito Hendriks <0310941@student.rocvantwente.nl>
2022-06-13 11:39:06 +0200

    Image auteur toegevoegd aan berichten tabel

--- import.sql
+++ import.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE `nu`;
 
-/* Rank is redacteur of niks */
+/* Rank administrator = 2, redacteur = 1, gebruiker = 0 */
 CREATE TABLE `users` (
     id MEDIUMINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     voornaam varchar(100) NOT NULL,
@@ -19,5 +19,6 @@ CREATE TABLE `berichten` (
     datum_aangepast datetime NOT NULL,
     auteur varchar(100) NOT NULL,
     images varchar(255) NOT NULL,
+    image_auteur varchar(255) NOT NULL,
     categorie ENUM('Binnenland', 'Buitenland', 'Politiek', 'Klimaat', 'Video', 'Geld', 'Werk', 'Ondernemen', 'Onderweg', 'Voetbal', 'Formule1', 'SportOverig', 'Games', 'Reviews', 'FilmsEnSeries', 'Muziek', 'BoekEnCultuur', 'Koningshuis') NOT NULL
 );
