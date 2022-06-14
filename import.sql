64e157b90cd5f18de6e63462691eb5583c2d7be6
ThomasMulders <0341091@student.rocvantwente.nl>
2022-06-14 08:46:34 +0200

    User table geupdate

--- import.sql
+++ import.sql
@@ -5,7 +5,7 @@ CREATE TABLE `users` (
     id MEDIUMINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     voornaam varchar(100) NOT NULL,
     achternaam varchar(100) NOT NULL,
-    username char(100) NOT NULL UNIQUE,
+    email varchar(100) NOT NULL UNIQUE,
     passwords varchar(255) NOT NULL,
     rank varchar(100) NOT NULL
 );
