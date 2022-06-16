d2e5ed149d9642b34452ae04e1d7639fa1fadd7f
Fernando Kook <0337874@student.rocvantwente.nl>
2022-06-16 12:39:32 +0200

    CARLITOOOW

---
+++ loose/footer.php
@@ -0,0 +1,214 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="/Project2/style/footer.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&family=Oswald:wght@200;300;400;500;600;700&display=swap" rel="stylesheet">
+    <title>Document</title>
+</head>
+
+<body>
+    <div id="foot">
+        <footer>
+            <table>
+                <tr>
+                    <th><a class="textdeco" href="">Voorpagina</a></th>
+                    <th><a class="textdeco" style="color: blue;" href="">Economie</a></th>
+                    <th><a class="textdeco" style="color: red;" href="">Sport</a></th>
+                    <th><a class="textdeco" style="color: purple;" href="">Tech</a></th>
+                    <th><a class="textdeco" style="color: orange;" href="">Media en</a></th>
+                    <th><a class="textdeco" style="color: darkmagenta;" href="">Overig</a></th>
+                    <th><a class="textdeco" href="">Regio</a></th>
+                    <td><a class="textdeco" href="">Contact met de</a></td>
+                </tr>
+                <tr>
+                    <td><a class="textdeco" href="">Net Binnen</a></td>
+                    <td><a class="textdeco" href="">Klimaat</a></td>
+                    <td><a class="textdeco" href="">Voetbal</a></td>
+                    <td><a class="textdeco" href="">Games</a></td>
+                    <th><a class="textdeco" style="color: orange;" href="">Cultuur</a></th>
+                    <td><a class="textdeco" href="">Kind & Gezin</a></td>
+                    <td><a class="textdeco" href="">Alphen a/d Rijn</a></td>
+                    <td><a class="textdeco" href="">redactie</a></td>
+                </tr>
+                <tr>
+                    <td><a class="textdeco" href="">Meest gelezen</a></td>
+                    <td><a class="textdeco" href="">Wonen</a></td>
+                    <td><a class="textdeco" href="">Formule 1</a></td>
+                    <td><a class="textdeco" href="">Reviews</a></td>
+                    <td><a class="textdeco" href="">Films en Series</a></td>
+                    <td><a class="textdeco" href="">dieren</a></td>
+                    <td><a class="textdeco" href="">Amersfoort</a></td>
+                    <td><a class="textdeco" href="">Colofon</a></td>
+                </tr>
+                <tr>
+                    <td><a class="textdeco" href="">Oorlog in</a></td>
+                    <td><a class="textdeco" href="">Geld</a></td>
+                    <td><a class="textdeco" href="">Scorebord</a></td>
+                    <td><a class="textdeco" href="">Achtergrond</a></td>
+                    <td><a class="textdeco" href="">Muziek</a></td>
+                    <td><a class="textdeco" href="">Eten en</a></td>
+                    <td><a class="textdeco" href="">Amsterdam</a></td>
+                    <td><a class="textdeco" href="">Over NU.nl</a></td>
+                </tr>
+                <tr>
+                    <td><a class="textdeco" href="">Oekraine</a></td>
+                    <td><a class="textdeco" href="">Werk</a></td>
+                    <td><a class="textdeco" href="">Spellen</a></td>
+                    <td></td>
+                    <td><a class="textdeco" href="">Boek en Cultuur</a></td>
+                    <td><a class="textdeco" href="">Drinken</a></td>
+                    <td><a class="textdeco" href="">Arnhem</a></td>
+                    <td><a class="textdeco" href="">Copyright</a></td>
+                </tr>
+                <tr>
+                    <td><a class="textdeco" href="">Binnenland</a></td>
+                    <td><a class="textdeco" href="">Ondernemen</a></td>
+                    <td></td>
+                    <td></td>
+                    <td><a class="textdeco" href="">Media</a></td>
+                    <td><a class="textdeco" href="">Gezondheid</a></td>
+                    <td><a class="textdeco" href="">Breda</a></td>
+                    <td><a class="textdeco" href="">Disclaimer</a></td>
+                </tr>
+                <tr>
+                    <td><a class="textdeco" href="">Buitenland</a></td>
+                    <td><a class="textdeco" href="">Brexit</a></td>
+                    <td></td>
+                    <td></td>
+                    <td><a class="textdeco" href="">Achterklap</a></td>
+                    <td><a class="textdeco" href="">NUcheckt</a></td>
+                    <td><a class="textdeco" href="">Den Bosch</a></td>
+                    <td><a class="textdeco" href="">Privacy en</a></td>
+                </tr>
+                <tr>
+                    <td><a class="textdeco" href="">Algemeen</a></td>
+                    <td><a class="textdeco" href="">Auto</a></td>
+                    <td></td>
+                    <td></td>
+                    <td><a class="textdeco" href="">Koninghuis</a></td>
+                    <td><a class="textdeco" href="">Opmerkelijk</a></td>
+                    <td><a class="textdeco" href="">Den Haag</a></td>
+                    <td><a class="textdeco" href="">cookiebeleid</a></td>
+                </tr>
+                <tr>
+                    <td><a class="textdeco" href="">politiek</a></td>
+                    <td><a class="textdeco" href="">Aandelen</a></td>
+                    <td></td>
+                    <td></td>
+                    <td><a class="textdeco" href="">TV gids</a></td>
+                    <td><a class="textdeco" href="">Thuis</a></td>
+                    <td><a class="textdeco" href="">Ede</a></td>
+                    <td><a class="textdeco" href="">Cookie instellingen</a></td>
+                </tr>
+                <tr>
+                    <td><a class="textdeco" href="">Video</a></td>
+                    <td><a class="textdeco" href="">Verkeer</a></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td><a class="textdeco" href="">Vakantie</a></td>
+                    <td><a class="textdeco" href="">Eindhoven</a></td>
+                    <td><a class="textdeco" href="">Klachten / Feedback</a></td>
+                </tr>
+                <tr>
+                    <td><a class="textdeco" href="">Podcast</a></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td><a class="textdeco" href="">Wetenschap</a></td>
+                    <td><a class="textdeco" href="">Groningen</a></td>
+                    <td><a class="textdeco" href="">Toegankelijkheid</a></td>
+                </tr>
+                <tr>
+                    <td><a class="textdeco" href="">Weer</a></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td><a class="textdeco" href="">Haarlem</a></td>
+                    <td><a class="textdeco" href="">Adverteren</a></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td><a class="textdeco" href="">Leiden</a></td>
+                    <td><a class="textdeco" href="">Apps</a></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td><a class="textdeco" href="">Nijmegen</a></td>
+                    <td><a class="textdeco" href="">Werken bij NU.nl</a></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td><a class="textdeco" href="">Rotterdam</a></td>
+                    <td><a class="textdeco" href="">Verzekeringvergelijker</a></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td><a class="textdeco" href="">Tilburg</a></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td><a class="textdeco" href="">Utrecht</a></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td><a class="textdeco" href="">Zeeland</a></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td><a class="textdeco" href="">Zwolle</a></td>
+                    <td></td>
+                </tr>
+            </table>
+        </footer>
+    </div>
+</body>
+
+</html>
