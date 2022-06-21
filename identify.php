289d42e7775896ef8e7012286c70a7a80accc792
BramFrieling <0338181@student.rocvantwente.nl>
2022-06-21 10:23:17 +0200

    tijdelijke identify style aangepast

--- identify.php
+++ identify.php
@@ -10,7 +10,8 @@ include_once("config/config.php");
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Nu.NL login</title>
+    <link rel="icon" href="images/Nu-nl-logo.png">
+    <title>Inloggen | nu.nl</title>
     <link rel="stylesheet" href="identify.css">
     <link rel="stylesheet" href="header.css">
     <link rel="stylesheet" href="footer.css">
@@ -20,21 +21,37 @@ include_once("config/config.php");
 <?php include("header.php") ?>
     <div class="column">
         <img src="images/Nu.svg" alt="Nu" class="nuImage">
-        <h1>Login of maak je account aan</h1>
-        <span>vul je e-mailadres in</span>
+        <h1>Log in of maak je account aan</h1>
+        <span>Vul je e-mailadres in</span>
         <form class="emailInput" action="" method="POST">
             <input type="text" id="email" class="email" name="email"> <br>
             <button type="submit" class="button" name="submitEmail"><h3>Ga Verder</h3></button>
         </form>
         <summary class="summary">
-        <span class="dpgmedia">Over DPG Media account</span> <img src="images/arrow-left.svg" alt="pijltje" class="pijltje">
+        <button onclick="dropdownDPG()" class="dropdown_button"><span class="dpgmedia">Over DPG Media account</span> <img src="images/arrow-left.svg" alt="pijltje" id="pijltje"></button>
         </summary>
-        
-        <h2>Hulp bij inloggen</h2>
 
-        <a href="https://login.dpgmedia.nl/password/reset?client_id=nu-site-web">Ik ben mijn wachtwoord vergeten</a> <br>
-        <a href="">Ik weet niet of ik al een account heb</a> <br>
-        <a href="">Ik logde eerder in via Facebook of Google</a> <br>
+        <div id="dropdown_DPG">
+            <div class="dropdown_DPG_content">
+                <h1>Over DPG Media account</h1>
+                <p>Met een DPG Media account kun je inloggen op de websites en in de apps van deze merken van DPG Media:</p>
+                <ul>
+                    <li>NU.nl, het AD, de Volkskrant, Trouw, Het Parool, de Stentor, Tubantia, de Gelderlander, het ED, Brabants Dagblad, BN DeStem en de PZC</li>
+                    <li>Libelle, Veronica Superguide, Story, Nouveau, Kek Mama, AutoWeek, Ouders van Nu, vtwonen college, Stijlvol Wonen</li>
+                    <li>Tijdschrift.nl, Reclamefolder.nl</li>
+                    <li>Qmusic, Joe</li>
+                </ul>
+                <p>Wanneer je inlogt, krijg je toegang tot bepaalde extra's of mogelijkheden. Deze verschillen per merk.</p>
+                <p>Heb je een abonnement op een van de nieuwsmerken? Dan krijg je onbeperkt toegang tot alle artikelen en video's van dat nieuwsmerk.</p>
+            </div>
+        </div>
+        
+        <div class="help_login">
+            <h2>Hulp bij inloggen</h2>
+            <a href="https://login.dpgmedia.nl/password/reset?client_id=nu-site-web">Ik ben mijn wachtwoord vergeten</a> <br>
+            <a href="">Ik weet niet of ik al een account heb</a> <br>
+            <a href="">Ik logde eerder in via Facebook of Google</a> <br>
+        </div>
     </div>
     <?php
     //PHP Gedeelte
@@ -42,7 +59,7 @@ include_once("config/config.php");
         if ($_POST['email'] != "") {
             $emailAdress = $_POST['email'];
 
-            $execute = $pdo->prepare('SELECT * FROM users WHERE email=:email');
+            $execute = $conn->prepare('SELECT * FROM users WHERE email=:email');
 
             $data = [
                 ":email" => $emailAdress,
@@ -60,6 +77,7 @@ include_once("config/config.php");
         }
     }
     ?>
+    <script src="identify_script.js"></script>
 
     <?php include("footer.php") ?>
 </body>
