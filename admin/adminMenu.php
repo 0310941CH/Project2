2192cfff33964f5bba02f2fd7c2444469db668f7
Carlito Hendriks <0310941@student.rocvantwente.nl>
2022-06-28 09:29:00 +0200

    Admin panel volledig afgemaakt, logout bestand aangemaakt

--- admin/adminMenu.php
+++ admin/adminMenu.php
@@ -9,6 +9,8 @@
     <ul>
         <li><a href="../admin/artikelOverzicht.php">Artikel overzicht</a></li>
         <li><a href="../admin/artikelCreate.php">Artikel maken</a></li>
+        <li><a href="../index.php">Terug naar index</a></li>
+        <li><a href="../includes/logout.php">Uitloggen</a></li>
     </ul>
 
 </body>
